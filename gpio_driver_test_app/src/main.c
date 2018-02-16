d33435783be19fa981c0f4db58e0fb714b50a63c
sams3p10l <35175347+sams3p10l@users.noreply.github.com>
2018-02-16 02:02:18 +0100

    Test app final version upload

--- gpio_driver_test_app/src/main.c
+++ gpio_driver_test_app/src/main.c
@@ -4,33 +4,91 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <pthread.h>
 
 #define BUF_LEN 80
+
+int enable;
+double IIRiterations[2], result[2];
+double alpha = 1;
+char c;
+int iir_cnt = 0;
+int counter = 0;
+
+double IIR(double d)
+{
+	if(iir_cnt != 3)
+		iir_cnt++;
+	if((iir_cnt) == 1)
+	{
+		return d;
+	}
+	else if(iir_cnt == 2)
+	{
+		result[0] = d;
+		IIRiterations[0] = d;
+		return result[0];
+	}
+	IIRiterations[1] = d;
+	result[1] = alpha * (result[0] + IIRiterations[1] - IIRiterations[0]);
+    IIRiterations[0] = IIRiterations[1];
+    result[0] = result[1];
+
+    return result[1];
+}
 
 int main()
 {
     int file_desc;
-    int ret_val;
+    int a;
     char tmp[BUF_LEN];
+    double distance, filteredDistance;
 
-    /* Open dummy file. */
-    file_desc = open("dummy", O_RDWR);
+    IIRiterations[0] = 0;
+	result[0] = 0;
+	result[1] = 0;
 
-    if(file_desc < 0)
+    printf("Press ENTER to start measuring\n");
+    while(1)
     {
-        printf("Error, 'dummy' not opened\n");
-        return -1;
+        c = getchar();
+		if(c == 10)
+			break;
     }
 
-    /* Write to dummy file. */
-    ret_val = write(file_desc, tmp, BUF_LEN);
+    while (1)
+    {
+        file_desc = open("/dev/gpio_driver", O_RDWR);
+		if(file_desc < 0)
+		{
+			printf("Error, 'output' not opened\n");
+			return -1;
+		}
 
-    /* Read from dummy file. */
-    ret_val = read(file_desc, tmp, BUF_LEN);
+		memset(tmp, 0, BUF_LEN);
 
-    /* Close dummy file. */
-    close(file_desc);
-    
+        if(read(file_desc, tmp, BUF_LEN) != -1)
+        {
+            a = atoi(tmp);
+            distance = (double)a/58;
+			counter ++;
+        }
+        else
+        {
+            printf("File read error\n");
+            return -1;
+        }
+
+		filteredDistance = IIR(distance);
+		close(file_desc);
+
+		if(counter == 100)
+		{
+			printf("Distance: %.2lf cm\n", filteredDistance);
+			counter = 0;
+		}
+
+		usleep(500);
+    }
+
     return 0;
 }
